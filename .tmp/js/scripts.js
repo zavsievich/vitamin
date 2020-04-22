5ca658558f2e1673476e7ce9374daf1d4073f8a2
Matvey <lishay.matvey20001506@gmail.com>
2020-04-22 12:42:13 +0300

    added mobile menu and js for it

--- .tmp/js/scripts.js
+++ .tmp/js/scripts.js
@@ -1,5 +1,19 @@
 "use strict";
 
+// open menu
+function openMenu() {
+  var btnMenu = document.querySelector('.js-toggle');
+  var menuList = document.querySelector('.js-menu');
+  btnMenu.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    btnMenu.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+  }
+}
+
+openMenu(); // slick-slider
+
 $('.js-product-carousel').slick({
   arrows: false,
   dots: true,
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,20 @@
+// open menu
+function openMenu() {
+    const btnMenu = document.querySelector('.js-toggle');
+    const menuList = document.querySelector('.js-menu');
+    
+    btnMenu.addEventListener('click', toggleMenu);
+    
+    function toggleMenu() {
+        btnMenu.classList.toggle('is-active');
+        menuList.classList.toggle('is-active');
+    }
+}
+
+openMenu();
+
+// slick-slider
+
 $('.js-product-carousel').slick({
     arrows: false,
     dots: true,
